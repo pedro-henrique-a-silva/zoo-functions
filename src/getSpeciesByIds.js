1a3a278c2f7649e8b98debabe5c1f8794357cceb
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-06-12 18:52:58 -0300

    Requisito 01 concluido, implementação função getSpeciesById

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,6 +2,18 @@ const data = require('../data/zoo_data');
 
 const getSpeciesByIds = (...ids) => {
   // seu código aqui
+  const speciesName = [];
+  if (ids.length !== 0) {
+    ids.forEach((id) => {
+      const specieData = data
+        .species.find((specie) => specie.id === id);
+
+      if (specieData) speciesName.push(specieData);
+    });
+    return speciesName;
+  }
+  return [];
 };
 
+// console.log(getSpeciesByIds(frogsId, 'e8481c1d-42ea-4610-8e1-1752cfc05a46'));
 module.exports = getSpeciesByIds;
