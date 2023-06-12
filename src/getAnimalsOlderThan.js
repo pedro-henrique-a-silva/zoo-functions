21d73f95e04274bf3ff00ba736dfb6fa52b0bb9e
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-06-12 19:19:30 -0300

    Requisito 03 concluido, implementação função getEmployeeByName

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -4,6 +4,6 @@ const getAnimalsOlderThan = (animal, age) =>
   data.species.find(({ name }) => name === animal).residents
     .every((animalResidents) => animalResidents.age > age);
 
-console.log(getAnimalsOlderThan('bears', 3));
+// console.log(getAnimalsOlderThan('bears', 3));
 
 module.exports = getAnimalsOlderThan;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+  const { employees: employeeData } = data;
+  const employee = employeeData
+    .find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName);
+
+  return (employee === undefined) ? employee : {};
 };
+
+console.log(getEmployeeByName());
 
 module.exports = getEmployeeByName;
